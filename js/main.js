2953f56f49c623ef6b4e48d5c57571bf87be3bee
Walter Goins <wgoinz@gmail.com>
2018-12-02 12:42:44 -0500

    Added new js files for sliding page website. Branching before committing new changes with js.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// ADDED CODEPEN INFO FOR FUNCTION TO INITATE SLIDER 
+// https://codepen.io/alvarotrigo/pen/NxyPPp
+
+new fullpage('#fullpage', {
+    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+});
